f01be09c57aac646f91654d413d88ec5f326dd92
Ahmed Elkfafy <73217710+Elkfafy@users.noreply.github.com>
2022-09-30 16:15:09 +0200

    1

--- src/app/pages/single-product/single-product.component.ts
+++ src/app/pages/single-product/single-product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { GlobalService } from 'src/app/services/global.service';
 @Component({
   selector: 'app-single-product',
@@ -9,26 +10,40 @@ import { GlobalService } from 'src/app/services/global.service';
 export class SingleProductComponent implements OnInit {
   product: any;
   isLoading: boolean = true;
-  isMine = false
+  isMine = false;
   constructor(
     public global: GlobalService,
-    private activated: ActivatedRoute
+    private activated: ActivatedRoute,
+    private toastr: ToastrService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     const productId = this.activated.snapshot.paramMap.get('id');
     this.global.getSingleProduct(productId).subscribe(
       (res: any) => {
-        this.product = {...res.data};
-        if (this.product.vendorId == this.global.user._id) this.isMine = true
-        this.product.images = []
+        this.product = { ...res.data };
+        if (this.product.vendorId == this.global.user._id) this.isMine = true;
+        this.product.images = [];
         res.data.images.forEach((image: any) => {
-          this.product.images.push(this.global.backendUrl + image)
-        })
-        console.log(this.product.images)
+          this.product.images.push(this.global.backendUrl + image);
+        });
       },
       (e: any) => console.log(e),
       () => (this.isLoading = false)
+    );
+  }
+
+  handleDelete(id: any) {
+    this.global.deleteProduct(id).subscribe(
+      (res) => {
+        this.toastr.success('Product Deleted');
+        this.router.navigateByUrl('/products');
+      },
+      (e) => {
+        console.log(e);
+        this.toastr.error('server error');
+      }
     );
   }
 }
